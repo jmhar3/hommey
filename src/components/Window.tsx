005c5da7e4ef439e2b3ac4553c3e25741c5b3c8a
jmhar3 <jmhar@protonmail.com>
2026-09-05 19:55:02 +1000

    provide basic window layout

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,19 +1,34 @@
 import { Box, Flex, Stack, Title } from "@mantine/core";
 
 import type { Window as WindowType } from "../screens";
+import NavButton from "./NavButton";
 
 interface WindowProps {
   window: WindowType;
   setActiveWindow: (window: WindowType, isOpen: boolean) => void;
 }
 function Window(props: WindowProps) {
+  const minimiseWindow = () => {};
+
+  const maximiseWindow = () => {};
+
+  const closeWindow = () => {};
+
   return (
-    <Stack>
-      <Flex>
+    <Stack bg="brown" p="xs" bdrs="3" h="100%" gap="xs">
+      <Flex p="xs" bg="gold" w="100%" align="center" justify="space-between">
         <Title>{props.window.title}</Title>
+
+        <Flex gap="xs">
+          <NavButton label="-" onClick={minimiseWindow} />
+          <NavButton label="+" onClick={maximiseWindow} />
+          <NavButton label="x" onClick={closeWindow} />
+        </Flex>
       </Flex>
 
-      <Box>{props.window.screen}</Box>
+      <Box h="100%" bg="white">
+        {props.window.screen}
+      </Box>
     </Stack>
   );
 }
