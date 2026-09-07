b0416070ae4f530bba7468ac3499fbfe5a8c3689
jmhar3 <jmhar@protonmail.com>
2026-09-07 18:17:27 +1000

    build basic hydrangea layout

--- src/components/dashboard/Hydrangea.tsx
+++ src/components/dashboard/Hydrangea.tsx
@@ -1,9 +1,35 @@
-import { Stack, Text } from "@mantine/core";
+import { Button, Group, Image, Stack, Title } from "@mantine/core";
+
+import hydrangea from "../../../public/assets/hydrangea.png";
 
 function Hydrangea() {
   return (
-    <Stack>
-      <Text>Stay Hydrated</Text>
+    <Stack p="xs" bdrs="3" bd="solid 2px brown">
+      <Title c="cornflowerblue">Stay Hydrated with Hydrangea</Title>
+
+      <Image src={hydrangea} w="100%" h="100%" />
+
+      <Group grow gap="xs">
+        <Button
+          variant="outline"
+          color="cornflowerblue"
+          bd="solid 2px cornflowerblue"
+        >
+          Lil Drink
+        </Button>
+
+        <Button
+          variant="outline"
+          color="cornflowerblue"
+          bd="solid 2px cornflowerblue"
+        >
+          Big Drink
+        </Button>
+
+        <Button variant="outline" color="brown" bd="solid 2px brown">
+          Refill
+        </Button>
+      </Group>
     </Stack>
   );
 }
