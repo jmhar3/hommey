84d509fbe3c649cf02aedfb5659e7efc77e06341
jmhar3 <jmhar@protonmail.com>
2026-09-07 15:40:56 +1000

    convert dashboard to grid

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex, Stack, Text } from "@mantine/core";
+import { Flex, Grid, Stack, Text } from "@mantine/core";
 
 import InCamera from "../components/dashboard/InCamera";
 import Hydrangea from "../components/dashboard/Hydrangea";
@@ -9,31 +9,41 @@ import ShoppingList from "../components/dashboard/ShoppingList";
 
 function Dashboard() {
   return (
-    <Flex>
-      <Stack>
-        <Flex>
-          <Stack>
-            <Text>*insert todays date here*</Text>
-            <Text>Good Morning</Text>
-          </Stack>
-          <Text>*insert current weather: icon, temp, uv, chance of rain*</Text>
-        </Flex>
-        <Flex>
-          <Stack>
-            <InCamera />
-            <Hydrangea />
-          </Stack>
-          <Stack>
-            <Recommendations />
-            <Reminders />
-          </Stack>
-        </Flex>
-      </Stack>
-      <Stack>
-        <Quests />
-        <ShoppingList />
-      </Stack>
-    </Flex>
+    <Grid grow p="xs">
+      <Grid.Col span={8}>
+        <Grid grow p="xs">
+          <Grid.Col span={12}>
+            <Flex>
+              <Stack>
+                <Text>*insert todays date here*</Text>
+                <Text>Good Morning</Text>
+              </Stack>
+              <Text>
+                *insert current weather: icon, temp, uv, chance of rain*
+              </Text>
+            </Flex>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Stack>
+              <InCamera />
+              <Hydrangea />
+            </Stack>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Stack>
+              <Recommendations />
+              <Reminders />
+            </Stack>
+          </Grid.Col>
+        </Grid>
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Stack>
+          <Quests />
+          <ShoppingList />
+        </Stack>
+      </Grid.Col>
+    </Grid>
   );
 }
 
