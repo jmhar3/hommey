73e59b2ef85e3354140da08e24838940a894d484
jmhar3 <jmhar@protonmail.com>
2026-09-15 15:35:55 +1000

    add background colour to container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,12 @@
 import { Box } from "@mantine/core";
 
-import { lightInset } from "../helpers/theme";
+import { colours, lightInset } from "../helpers/theme";
 
 import type { PropsWithChildren } from "react";
 
 function Container({ children }: PropsWithChildren) {
   return (
-    <Box p="xs" w="100%" {...lightInset}>
+    <Box p="xs" w="100%" {...lightInset} bg={colours.white}>
       {children}
     </Box>
   );
