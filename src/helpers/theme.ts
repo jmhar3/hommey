d3008235f03a7299665fe76166f33e730084d696
jmhar3 <jmhar@protonmail.com>
2026-09-15 11:20:06 +1000

    theme helper

---
+++ src/helpers/theme.ts
@@ -0,0 +1,34 @@
+export const colours = {
+  light: "#F3EDE5",
+  mid: "#EAE0D7",
+  dark: "#281401",
+  contrast: "#F07800",
+};
+
+export const inset = {
+  bd: `solid 4px ${colours.dark}`,
+  style: {
+    boxShadow: "inset -1px -1px 0px 1px #3A58EF, inset 1px 1px 0px 1px crimson",
+  },
+};
+
+export const shadow = {
+  bd: `solid 4px ${colours.dark}`,
+  style: {
+    boxShadow: "1px 1px 0px 1px #3A58EF, -1px -1px 0px 1px crimson",
+  },
+};
+
+export const lightInset = {
+  bd: `solid 4px ${colours.dark}`,
+  style: {
+    boxShadow: `inset -3px -3px 0px 1px ${colours.mid}`,
+  },
+};
+
+export const lightShadow = {
+  bd: `solid 4px ${colours.dark}`,
+  style: {
+    boxShadow: `3px 3px 0px 1px ${colours.mid}`,
+  },
+};
