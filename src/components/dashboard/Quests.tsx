413070cd59f51aad5b5ea1669cc1d7a1168edf77
jmhar3 <jmhar@protonmail.com>
2026-09-15 15:38:29 +1000

    populate quests component with db data

--- src/components/dashboard/Quests.tsx
+++ src/components/dashboard/Quests.tsx
@@ -1,24 +1,49 @@
+import { useEffect, useMemo } from "react";
 import { Box, Group, Stack, Text, Title } from "@mantine/core";
 
 import Container from "../Container";
 
+import { useAppDispatch, useAppSelector } from "../../state/hooks";
+import { fetchQuests } from "../../state/quests/questsThunks";
+
+import {
+  selectQuests,
+  selectQuestsStatus,
+} from "../../state/quests/questsSlice";
+import dayjs from "dayjs";
+
 function Quests() {
+  const dispatch = useAppDispatch();
+
+  const questsStatus = useAppSelector(selectQuestsStatus);
+  const quests = useAppSelector(selectQuests);
+
+  useEffect(() => {
+    if (questsStatus === "idle") {
+      dispatch(fetchQuests());
+    }
+  }, [dispatch, questsStatus]);
+
+  const dueQuests = useMemo(
+    () =>
+      quests.filter(({ last_completed_at, frequency }) => {
+        const today = dayjs();
+        const lastCompletedAt = dayjs(last_completed_at);
+        const nextDueAt = lastCompletedAt.add(frequency, "day");
+        return nextDueAt.isBefore(today) || nextDueAt.isSame(today, "day");
+      }),
+    [quests],
+  );
+
   return (
     <Container>
       <Stack>
         <Title>Quests</Title>
 
         <Stack>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
-          <Text>This is a quest</Text>
+          {dueQuests.map((quest) => (
+            <Text key={quest.id}>{quest.label}</Text>
+          ))}
         </Stack>
 
         <Group grow gap="xs">
