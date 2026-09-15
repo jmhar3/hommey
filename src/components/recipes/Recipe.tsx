202f2a75e4e1c6616352b48bb8783e9fc8e32fae
jmhar3 <jmhar@protonmail.com>
2026-09-15 11:21:52 +1000

    add icon buttons for recipe actions

--- src/components/recipes/Recipe.tsx
+++ src/components/recipes/Recipe.tsx
@@ -1,18 +1,40 @@
 import { ActionIcon, Badge, Flex, List, Stack, Title } from "@mantine/core";
+import { FaCartPlus, FaEdit, FaStar } from "react-icons/fa";
+
 import type { Recipe as RecipeType } from "../../state/types";
 
 function Recipe(recipe: RecipeType) {
+  const onFavourite = () => {};
+
   return (
     <Stack>
-      <Flex justify="space-between">
+      <Flex align="center" justify="space-between">
         <Title>{recipe.title}</Title>
-        <ActionIcon variant="transparent" />
+
+        <Flex align="center" gap="xs">
+          <ActionIcon
+            onClick={onFavourite}
+            color={recipe.favourite ? "gold" : "grey"}
+          >
+            <FaStar />
+          </ActionIcon>
+
+          <ActionIcon color="brown">
+            <FaCartPlus />
+          </ActionIcon>
+
+          <ActionIcon color="brown">
+            <FaEdit />
+          </ActionIcon>
+        </Flex>
       </Flex>
 
       {recipe.tags && (
-        <Flex>
+        <Flex gap="xs">
           {recipe.tags.map((tag) => (
-            <Badge key={tag}>{tag}</Badge>
+            <Badge key={tag} radius="xs">
+              {tag}
+            </Badge>
           ))}
         </Flex>
       )}
