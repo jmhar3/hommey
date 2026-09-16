830606dae48a433b6ef2d0c934f69f775f0f5e0f
jmhar3 <jmhar@protonmail.com>
2026-09-16 13:24:43 +1000

    only show list view on dashboard

--- src/components/dashboard/ShoppingList.tsx
+++ src/components/dashboard/ShoppingList.tsx
@@ -1,24 +1,17 @@
 import { Divider, Stack, Title } from "@mantine/core";
 
 import Container from "../Container";
-import ShoppingListItems from "../ShoppingListItems";
-import ShoppingListItemForm from "../ShoppingListItemForm";
+import ShoppingListItems from "../shoppingList/ShoppingListItems";
 
 import { colours } from "../../helpers/theme";
 
 function ShoppingList() {
   return (
     <Container>
-      <Stack>
+      <Stack pb="xs">
         <Title>Shopping List</Title>
 
-        <Divider orientation="vertical" bd={`2px solid ${colours.contrast}`} />
-
-        <ShoppingListItems />
-
-        <Divider orientation="vertical" bd={`2px solid ${colours.contrast}`} />
-
-        <ShoppingListItemForm />
+        <ShoppingListItems view="list" />
       </Stack>
     </Container>
   );
