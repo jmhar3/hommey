b9289d1b8dd0c0859cca0ece2d9f591444f3c597
jmhar3 <jmhar@protonmail.com>
2026-09-16 18:30:18 +1000

    remove double border

--- src/screens/Clock.tsx
+++ src/screens/Clock.tsx
@@ -1,10 +1,8 @@
 import { Stack } from "@mantine/core";
 
-import { colours, shadow } from "../helpers/theme";
-
 function Clock() {
   return (
-    <Stack p="xs" h="100vh" gap="xs" bg={colours.mid} {...shadow}>
+    <Stack p="xs" h="100vh" gap="xs">
       Clock
     </Stack>
   );
