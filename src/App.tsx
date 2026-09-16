e3b7d01a046289f8aa59aa22322110d3f95e25d9
jmhar3 <jmhar@protonmail.com>
2026-09-16 18:14:57 +1000

    adjust page height + padding

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <Stack w="100vw" h="100vh" bg={colours.light} gap="0">
       <BackgroundImage src={images[randomImageNum]}>
-        <Center h="93vh" p="sm">
+        <Center h="92vh" p="xs">
           {focusedWindow && (
             <Window
               window={focusedWindow}
