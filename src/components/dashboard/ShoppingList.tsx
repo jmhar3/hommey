4fd5aea088d09114c6281b37cb92f32504ab1d02
jmhar3 <jmhar@protonmail.com>
2026-09-16 17:23:51 +1000

    add button + form to add new item

--- src/components/dashboard/ShoppingList.tsx
+++ src/components/dashboard/ShoppingList.tsx
@@ -1,15 +1,31 @@
-import { Stack, Title } from "@mantine/core";
+import { Button, Divider, Stack, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 import Container from "../Container";
 import ShoppingListItems from "../shoppingList/ShoppingListItems";
+import ShoppingListItemForm from "../shoppingList/ShoppingListItemForm";
+
+import { colours, contrastInset } from "../../helpers/theme";
 
 function ShoppingList() {
+  const [showForm, { open, close }] = useDisclosure();
+
   return (
     <Container>
       <Stack pb="xs">
         <Title>Shopping List</Title>
 
         <ShoppingListItems view="list" />
+
+        <Divider bd={`2px solid ${colours.contrast}`} />
+
+        {showForm ? (
+          <ShoppingListItemForm onComplete={close} />
+        ) : (
+          <Button {...contrastInset} onClick={open}>
+            ADD NEW ITEM
+          </Button>
+        )}
       </Stack>
     </Container>
   );
