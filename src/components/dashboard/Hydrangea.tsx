3be59631ef30d30cf8851de4cea4d13cc9386e53
jmhar3 <jmhar@protonmail.com>
2026-09-16 17:25:59 +1000

    handle drink/refill clicks

--- src/components/dashboard/Hydrangea.tsx
+++ src/components/dashboard/Hydrangea.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { Button, Group, Image, Progress, Stack } from "@mantine/core";
 
 import Container from "../Container";
 
 import { useAppDispatch, useAppSelector } from "../../state/hooks";
-import { fetchHydrangea } from "../../state/hydrangea/hydrangeaThunks";
+
+import {
+  fetchHydrangea,
+  insertHydrangea,
+} from "../../state/hydrangea/hydrangeaThunks";
 
 import {
   selectHydrangea,
@@ -14,6 +18,11 @@ import {
 import { colours, contrastInset, lightInset } from "../../helpers/theme";
 
 import hydrangea from "../../../public/assets/hydrangea.png";
+
+const refill = 1500;
+const lilDrink = 300;
+const bigDrink = 1100;
+const totalCapacity = 12000;
 
 function Hydrangea() {
   const dispatch = useAppDispatch();
@@ -27,49 +36,69 @@ function Hydrangea() {
     }
   }, [dispatch, hydrangeaStatus]);
 
+  const hydrangeaPercent = useMemo(() => {
+    if (hydrangeaData) return (hydrangeaData.value / totalCapacity) * 100;
+  }, [hydrangeaData]);
+
+  const onDrink = (
+    type: "lil_drink" | "big_drink" | "refill",
+    value: number,
+  ) => {
+    dispatch(insertHydrangea({ type: type, value: value }));
+  };
+
   return (
     <Container>
       <Stack gap="xs">
         <Stack gap="5">
           <Image src={hydrangea} w="100%" h="100%" {...lightInset} />
 
-          {hydrangeaData && (
+          {hydrangeaPercent && (
             <Progress
-              size="lg"
+              size="xl"
               radius={0}
               bg={colours.mid}
               color={colours.contrast}
               transitionDuration={200}
-              value={hydrangeaData.value}
-              bd={`solid 2px ${colours.dark}`}
+              bd={`solid 4px ${colours.dark}`}
+              value={hydrangeaPercent}
             />
           )}
         </Stack>
 
-        <Group grow gap="xs">
+        <Group grow gap="xs" w="100%">
           <Button
+            p="0"
+            h="44px"
             {...contrastInset}
             style={{
               boxShadow: `inset -3px -3px 0px 1px ${colours.blue}`,
             }}
+            onClick={() => onDrink("lil_drink", hydrangeaData.value - lilDrink)}
           >
             LIL DRINK
           </Button>
 
           <Button
+            p="0"
+            h="44px"
             {...contrastInset}
             style={{
               boxShadow: `inset -3px -3px 0px 1px ${colours.blue}`,
             }}
+            onClick={() => onDrink("big_drink", hydrangeaData.value - bigDrink)}
           >
             BIG DRINK
           </Button>
 
           <Button
+            p="0"
+            h="44px"
             {...contrastInset}
             style={{
               boxShadow: "inset -3px -3px 0px 1px crimson",
             }}
+            onClick={() => onDrink("refill", hydrangeaData.value + refill)}
           >
             REFILL
           </Button>
