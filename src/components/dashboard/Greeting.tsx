5089e137e5d0725dd92330bc14994a5588bd410f
jmhar3 <jmhar@protonmail.com>
2026-09-16 20:19:37 +1000

    style weather snippets

--- src/components/dashboard/Greeting.tsx
+++ src/components/dashboard/Greeting.tsx
@@ -1,24 +1,35 @@
 import { useMemo } from "react";
-import { FaCloud, FaSun, FaThermometerHalf } from "react-icons/fa";
+import { Flex, Text, Stack, Title, Divider, Button } from "@mantine/core";
+import { FaCloudRain, FaSun, FaThermometerHalf, FaWind } from "react-icons/fa";
 import advancedFormat from "dayjs/plugin/advancedFormat";
 import dayjs from "dayjs";
 
-import {
-  ActionIcon,
-  Divider,
-  Flex,
-  Grid,
-  Stack,
-  Text,
-  Title,
-} from "@mantine/core";
-
 import Container from "../Container";
-import { colours } from "../../helpers/theme";
+
+import { colours, contrastShadow } from "../../helpers/theme";
+
+// import { useAppDispatch, useAppSelector } from "../../state/hooks";
+// import { fetchWeather } from "../../state/weather/weatherThunks";
+
+// import {
+//   selectWeather,
+//   selectWeatherStatus,
+// } from "../../state/weather/weatherSlice";
 
 dayjs.extend(advancedFormat);
 
 function Greeting() {
+  // const dispatch = useAppDispatch();
+
+  // const weatherStatus = useAppSelector(selectWeatherStatus);
+  // const weatherData = useAppSelector(selectWeather);
+
+  // useEffect(() => {
+  //   if (weatherStatus === "idle") {
+  //     dispatch(fetchWeather());
+  //   }
+  // }, [dispatch, weatherStatus]);
+
   const date = dayjs().format("dddd, Do of MMMM");
 
   const greeting = useMemo(() => {
@@ -35,38 +46,48 @@ function Greeting() {
 
   return (
     <Container>
-      <Grid>
-        <Grid.Col span={6}>
-          <Flex>
-            <Stack gap="0">
-              <Text size="1em">{date.toUpperCase()}</Text>
-              <Title size="2.4em">{greeting}</Title>
-            </Stack>
+      <Flex w="100%" align="center" gap="xs">
+        <Stack gap="0" w="100%">
+          <Text size="1em">{date.toUpperCase()}</Text>
+          <Title size="2.4em">{greeting}</Title>
+        </Stack>
 
-            <Divider
-              mx="xl"
-              size="lg"
-              color={colours.contrast}
-              orientation="vertical"
-            />
-          </Flex>
-        </Grid.Col>
+        <Divider size="lg" color={colours.contrast} orientation="vertical" />
 
-        <Grid.Col span={6}>
+        <Flex w="fit-content" gap="xs">
           {/*INSERT CURRENT WEATHER: ICONS, TEMP, UV, CHANCE OF RAIN*/}
-          <ActionIcon>
-            <FaThermometerHalf />
-          </ActionIcon>
+          <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
+            <Stack gap="xs" align="center" justify="center">
+              <FaThermometerHalf size="2.4em" />
+              <Text size="1.5em">
+                28°C
+                {/*{weatherData?.hourly.apparent_temperature}°C*/}
+              </Text>
+            </Stack>
+          </Button>
 
-          <ActionIcon>
-            <FaSun />
-          </ActionIcon>
+          <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
+            <Stack gap="xs" align="center" justify="center">
+              <FaWind size="2.4em" />
+              <Text size="1.5em">28k</Text>
+            </Stack>
+          </Button>
 
-          <ActionIcon>
-            <FaCloud />
-          </ActionIcon>
-        </Grid.Col>
-      </Grid>
+          <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
+            <Stack gap="xs" align="center" justify="center">
+              <FaSun size="2.4em" />
+              <Text size="1.5em">2UV</Text>
+            </Stack>
+          </Button>
+
+          <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
+            <Stack gap="xs" align="center" justify="center">
+              <FaCloudRain size="2.4em" />
+              <Text size="1.5em">2mm</Text>
+            </Stack>
+          </Button>
+        </Flex>
+      </Flex>
     </Container>
   );
 }
