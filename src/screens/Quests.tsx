6a318195f77fe3a15651b0be261d8148274d26a4
jmhar3 <jmhar@protonmail.com>
2026-09-17 12:39:47 +1000

    roughly template quests screen

--- src/screens/Quests.tsx
+++ src/screens/Quests.tsx
@@ -1,28 +1,37 @@
-import { Box, Group, Stack } from "@mantine/core";
+import { Box, Grid, Group, Stack } from "@mantine/core";
+
+import Quests from "../components/dashboard/Quests";
 
 import { colours, shadow } from "../helpers/theme";
 
-function Quests() {
+function BossBattle() {
   return (
-    <Stack p="xs" h="100vh" gap="xs" bg={colours.mid} {...shadow}>
-      Quests
-      <Group grow gap="xs">
-        <Box h="12.5em" bg={colours.light} {...shadow}>
-          BOSS
-        </Box>
+    <Stack p="xs" h="100vh" gap="xs">
+      <Grid>
+        <Grid.Col span={4}>
+          <Quests />
+        </Grid.Col>
 
-        <Stack gap="xs">
-          <Box h="6em" bg={colours.light} {...shadow}>
-            Wah
-          </Box>
+        <Grid.Col span={8}>
+          <Group grow gap="xs">
+            <Box h="12.5em" bg={colours.light} {...shadow}>
+              BOSS
+            </Box>
 
-          <Box h="6em" bg={colours.light} {...shadow}>
-            Fae
-          </Box>
-        </Stack>
-      </Group>
+            <Stack gap="xs">
+              <Box h="6em" bg={colours.light} {...shadow}>
+                Wah
+              </Box>
+
+              <Box h="6em" bg={colours.light} {...shadow}>
+                Fae
+              </Box>
+            </Stack>
+          </Group>
+        </Grid.Col>
+      </Grid>
     </Stack>
   );
 }
 
-export default Quests;
+export default BossBattle;
