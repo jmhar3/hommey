23325524cfb81fc7f1550e31d0a253b0c5f0fe42
jmhar3 <jmhar@protonmail.com>
2026-09-17 12:38:35 +1000

    enforce cohesive design

--- src/components/dashboard/InCamera.tsx
+++ src/components/dashboard/InCamera.tsx
@@ -2,17 +2,17 @@ import { Box, Group, Stack } from "@mantine/core";
 
 import Container from "../Container";
 
-import { colours, shadow } from "../../helpers/theme";
+import { contrastShadow } from "../../helpers/theme";
 
 function InCamera() {
   return (
     <Container>
       <Stack>
         <Group grow>
-          <Box p="xs" h="8em" w="6em" bg={colours.light} {...shadow}>
+          <Box p="xs" h="8em" w="6em" {...contrastShadow}>
             Harmon Phoenix II in the Nikon F55
           </Box>
-          <Box p="xs" h="8em" w="6em" bg={colours.light} {...shadow}>
+          <Box p="xs" h="8em" w="6em" {...contrastShadow}>
             Film in the Camera
           </Box>
         </Group>
