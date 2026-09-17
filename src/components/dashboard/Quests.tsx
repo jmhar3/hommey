34016c242c80b2fbdd9d34de294678995ab1e0fb
jmhar3 <jmhar@protonmail.com>
2026-09-17 12:38:48 +1000

    format quests + handle quest completion

--- src/components/dashboard/Quests.tsx
+++ src/components/dashboard/Quests.tsx
@@ -1,16 +1,20 @@
 import dayjs from "dayjs";
 import { useEffect, useMemo } from "react";
-import { Stack, Text, Title } from "@mantine/core";
+import { Box, Button, ScrollArea, Stack, Title } from "@mantine/core";
 
 import Container from "../Container";
 
 import { useAppDispatch, useAppSelector } from "../../state/hooks";
-import { fetchQuests } from "../../state/quests/questsThunks";
+import { fetchQuests, upsertQuests } from "../../state/quests/questsThunks";
 
 import {
   selectQuests,
   selectQuestsStatus,
 } from "../../state/quests/questsSlice";
+
+import { contrastShadow, lightInset } from "../../helpers/theme";
+
+import type { Quest } from "../../state/types";
 
 function Quests() {
   const dispatch = useAppDispatch();
@@ -24,6 +28,14 @@ function Quests() {
     }
   }, [dispatch, questsStatus]);
 
+  const completedQuests = useMemo(
+    () =>
+      quests.filter(({ last_completed_at }) => {
+        return dayjs(last_completed_at).isSame(dayjs(), "day");
+      }),
+    [quests],
+  );
+
   const dueQuests = useMemo(
     () =>
       quests.filter(({ last_completed_at, frequency }) => {
@@ -35,16 +47,60 @@ function Quests() {
     [quests],
   );
 
+  const healQuests = useMemo(
+    () => dueQuests.filter(({ type }) => type === "heal"),
+    [dueQuests],
+  );
+
+  const attackQuests = useMemo(
+    () => dueQuests.filter(({ type }) => type === "attack"),
+    [dueQuests],
+  );
+
+  const powerUpQuests = useMemo(
+    () => dueQuests.filter(({ type }) => type === "power_up"),
+    [dueQuests],
+  );
+
+  const completeQuest = (quest: Quest) => {
+    dispatch(
+      upsertQuests([{ ...quest, last_completed_at: dayjs().toISOString() }]),
+    );
+  };
+
   return (
     <Container>
-      <Stack>
-        <Title>Quests</Title>
+      <Stack gap="xs">
+        <Title>Quest Log</Title>
 
-        <Stack>
-          {dueQuests.map((quest) => (
-            <Text key={quest.id}>{quest.label}</Text>
-          ))}
-        </Stack>
+        <Box {...lightInset}>
+          <ScrollArea h="62vh">
+            <Stack p="xs">
+              {[...healQuests, ...powerUpQuests, ...attackQuests].map(
+                (quest) => (
+                  <Button
+                    key={quest.id}
+                    onClick={() => completeQuest(quest)}
+                    {...contrastShadow}
+                  >
+                    {quest.label.toUpperCase()}
+                  </Button>
+                ),
+              )}
+
+              {completedQuests.map((quest) => (
+                <Button
+                  key={quest.id}
+                  disabled
+                  {...lightInset}
+                  td="line-through"
+                >
+                  {quest.label.toUpperCase()}
+                </Button>
+              ))}
+            </Stack>
+          </ScrollArea>
+        </Box>
       </Stack>
     </Container>
   );
