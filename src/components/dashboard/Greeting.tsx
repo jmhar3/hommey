a22a3e691497b2d43ebbc1058bb3bed2d94c0fab
jmhar3 <jmhar@protonmail.com>
2026-09-17 12:38:24 +1000

    combine recommendations widget with greeting

--- src/components/dashboard/Greeting.tsx
+++ src/components/dashboard/Greeting.tsx
@@ -1,6 +1,23 @@
-import { useMemo } from "react";
-import { Flex, Text, Stack, Title, Divider, Button } from "@mantine/core";
-import { FaCloudRain, FaSun, FaThermometerHalf, FaWind } from "react-icons/fa";
+import { useMemo, useState } from "react";
+import {
+  Flex,
+  Text,
+  Stack,
+  Title,
+  Divider,
+  Button,
+  ActionIcon,
+  Group,
+} from "@mantine/core";
+import {
+  FaBrain,
+  FaCloudRain,
+  FaHandHoldingHeart,
+  FaHeart,
+  FaSun,
+  FaThermometerHalf,
+  FaWind,
+} from "react-icons/fa";
 import advancedFormat from "dayjs/plugin/advancedFormat";
 import dayjs from "dayjs";
 
@@ -30,6 +47,8 @@ function Greeting() {
   //   }
   // }, [dispatch, weatherStatus]);
 
+  const [selection, setSelection] = useState<"chill" | "brain" | "body">();
+
   const date = dayjs().format("dddd, Do of MMMM");
 
   const greeting = useMemo(() => {
@@ -46,48 +65,90 @@ function Greeting() {
 
   return (
     <Container>
-      <Flex w="100%" align="center" gap="xs">
-        <Stack gap="0" w="100%">
-          <Text size="1em">{date.toUpperCase()}</Text>
-          <Title size="2.4em">{greeting}</Title>
-        </Stack>
+      <Stack gap="xs">
+        <Flex w="100%" align="center" gap="xs">
+          <Stack gap="0" w="100%">
+            <Text size="1em">{date.toUpperCase()}</Text>
+            <Title size="2.4em">{greeting}</Title>
+            <Text pt="xs">HOW ARE YOU FEELING?</Text>
+          </Stack>
 
-        <Divider size="lg" color={colours.contrast} orientation="vertical" />
+          <Divider size="lg" color={colours.contrast} orientation="vertical" />
 
-        <Flex w="fit-content" gap="xs">
-          {/*INSERT CURRENT WEATHER: ICONS, TEMP, UV, CHANCE OF RAIN*/}
-          <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
-            <Stack gap="xs" align="center" justify="center">
-              <FaThermometerHalf size="2.4em" />
-              <Text size="1.5em">
-                28°C
-                {/*{weatherData?.hourly.apparent_temperature}°C*/}
-              </Text>
-            </Stack>
-          </Button>
+          <Flex w="fit-content" gap="xs">
+            {/*INSERT CURRENT WEATHER: ICONS, TEMP, UV, CHANCE OF RAIN*/}
+            <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
+              <Stack gap="xs" align="center" justify="center">
+                <FaThermometerHalf size="2.4em" />
+                <Text size="1.5em">
+                  28°C
+                  {/*{weatherData?.hourly.apparent_temperature}°C*/}
+                </Text>
+              </Stack>
+            </Button>
 
-          <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
-            <Stack gap="xs" align="center" justify="center">
-              <FaWind size="2.4em" />
-              <Text size="1.5em">28k</Text>
-            </Stack>
-          </Button>
+            <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
+              <Stack gap="xs" align="center" justify="center">
+                <FaWind size="2.4em" />
+                <Text size="1.5em">28k</Text>
+              </Stack>
+            </Button>
 
-          <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
-            <Stack gap="xs" align="center" justify="center">
-              <FaSun size="2.4em" />
-              <Text size="1.5em">2UV</Text>
-            </Stack>
-          </Button>
+            <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
+              <Stack gap="xs" align="center" justify="center">
+                <FaSun size="2.4em" />
+                <Text size="1.5em">2UV</Text>
+              </Stack>
+            </Button>
 
-          <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
-            <Stack gap="xs" align="center" justify="center">
-              <FaCloudRain size="2.4em" />
-              <Text size="1.5em">2mm</Text>
-            </Stack>
-          </Button>
+            <Button pb="0" pt="xs" px="xs" h="fit-content" {...contrastShadow}>
+              <Stack gap="xs" align="center" justify="center">
+                <FaCloudRain size="2.4em" />
+                <Text size="1.5em">2mm</Text>
+              </Stack>
+            </Button>
+          </Flex>
         </Flex>
-      </Flex>
+
+        <Stack gap="xs">
+          {selection ? (
+            <>
+              {selection === "chill" && <Text>Chill</Text>}
+              {selection === "brain" && <Text>Brain</Text>}
+              {selection === "body" && <Text>Body</Text>}
+            </>
+          ) : (
+            <Group grow>
+              <ActionIcon
+                p="xs"
+                h="6em"
+                onClick={() => setSelection("chill")}
+                {...contrastShadow}
+              >
+                <FaHandHoldingHeart size="lg" />
+              </ActionIcon>
+
+              <ActionIcon
+                p="xs"
+                h="6em"
+                onClick={() => setSelection("brain")}
+                {...contrastShadow}
+              >
+                <FaBrain size="lg" />
+              </ActionIcon>
+
+              <ActionIcon
+                p="xs"
+                h="6em"
+                onClick={() => setSelection("body")}
+                {...contrastShadow}
+              >
+                <FaHeart size="lg" />
+              </ActionIcon>
+            </Group>
+          )}
+        </Stack>
+      </Stack>
     </Container>
   );
 }
